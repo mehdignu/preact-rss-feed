399739a849603132ed4225e9e53eed609130bf3e
mehdi dridi <mehdignu@users.noreply.github.com>
2019-03-16 15:58:51 +0100

    change structure

--- src/components/app.js
+++ src/components/app.js
@@ -4,8 +4,7 @@ import { Router } from 'preact-router';
 import Header from './header';
 
 // Code-splitting is automated for routes
-import Home from '../routes/home';
-import Profile from '../routes/profile';
+import Category from '../routes/category';
 
 export default class App extends Component {
 	
@@ -22,9 +21,8 @@ export default class App extends Component {
 			<div id="app">
 				<Header />
 				<Router onChange={this.handleRoute}>
-					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
+					<Category path="/" cat="all" />
+					<Category path="/:cat" />
 				</Router>
 			</div>
 		);
--- src/routes/category/index.js
+++ src/routes/category/index.js
@@ -1,7 +1,7 @@
 import { h, Component } from 'preact';
 import style from './style';
 
-export default class Profile extends Component {
+export default class Category extends Component {
 	state = {
 		time: Date.now(),
 		count: 10
@@ -18,6 +18,7 @@ export default class Profile extends Component {
 
 	// gets called when this route is navigated to
 	componentDidMount() {
+		console.log('dede');
 		// start a timer for the clock:
 		this.timer = setInterval(this.updateTime, 1000);
 	}
@@ -28,11 +29,11 @@ export default class Profile extends Component {
 	}
 
 	// Note: `user` comes from the URL, courtesy of our router
-	render({ user }, { time, count }) {
+	render({ cat }, { time, count }) {
 		return (
 			<div class={style.profile}>
-				<h1>Profile: {user}</h1>
-				<p>This is the user profile for a user named { user }.</p>
+				<h1>Category: {cat}</h1>
+				<p>This is the user Category for a user named { cat }.</p>
 
 				<div>Current time: {new Date(time).toLocaleString()}</div>
 
