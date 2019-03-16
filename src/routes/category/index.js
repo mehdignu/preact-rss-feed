168cba6829a05534dc459ef503d403be67a6e4ec
mehdi dridi <dridignu@gmail.com>
2019-03-16 23:22:21 +0100

    Update index.js

--- src/routes/category/index.js
+++ src/routes/category/index.js
@@ -6,38 +6,7 @@ import * as actions from '../../store/actions';
 
 @connect(reduce, actions)
 export default class Category extends Component {
-	state = {
-		time: Date.now(),
-		count: 10
-	};
-
-	// update the current time
-	updateTime = () => {
-		this.setState({ time: Date.now() });
-	};
-
-	increment = () => {
-		this.setState({ count: this.state.count + 1 });
-	};
-
-
-	// gets called when this route is navigated to
-	componentWillMount() {
-
-
-	}
-
-	componentDidMount() {
-
-		// start a timer for the clock:
-		this.timer = setInterval(this.updateTime, 1000);
-	}
-
-	// gets called just before navigating away from the route
-	componentWillUpdate() {
-
-		clearInterval(this.timer);
-	}
+	
 
 	render({ cat }) {
 
